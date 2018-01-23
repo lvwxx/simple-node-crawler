894273ea473bd2efa1aee5085c2f768489a502ae
weibu <365881100@qq.com>
2018-01-23 14:35:23 +0800

    v

---
+++ node/index.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const request = require('request');
+const fs = require('fs');
+
+let movieDir = path.join(__dirname, '../movie-name/movie.txt');
+
+// 读取文件名称
+const readFiles = () => {
+    return new Promise((reslove,reject) => {
+        fs.readFile(movieDir, 'utf8', (err,data) => {
+            if(err) throw err;
+            console.log(data);
+            reslove(data);
+        })
+    });
+};
+
+// 获取海报
+const getPoster = (movieName) => {
+    let url = `https://api.douban.com/v2/movie/search?q=${encodeURI(movieName)}`;
+
+    return new Promise((reslove, reject) => {
+        request({url,json:true}, (err,resp,body) => {
+            if(err) return reject(err);
+
+            reslove(body.subjects[0].images.large);
+        });
+    });
+};
+
+// 保存海报
+const saveImg = (movieName, url) => {
+    request.get(url).pipe(fs.createWriteStream(path.join(__dirname, '../movie-img/' + movieName +'.jpg')));
+};
+
+
+
+(async () => {
+    let name = await readFiles();
+
+    console.log('=====');
+
+    saveImg(name, await getPoster(name));
+
+})();
